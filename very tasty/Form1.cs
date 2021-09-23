a4742d4a147dd822b2b4fa0897e459b73ee827ef
Gerson Jair Solano Villegas <USIS051921@ugb.edu.sv>
2021-09-23 11:24:46 -0500

    agregamos el login con acces

--- very tasty/Form1.cs
+++ very tasty/Form1.cs
@@ -6,6 +6,7 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Data.OleDb;
+using System.IO;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -37,22 +38,22 @@ namespace very_tasty
 
         private void button1_Click(object sender, EventArgs e)
         {
-            string consulta = "select password,usuario from login where password '" + textBox2 + "' and usuario '" + textBox1 + "';";
-            OleDbCommand comando = new OleDbCommand(consulta, conexion);
+            string consulta = "select password,usuario from login where password = '" + textBox2.Text + "' and usuario = '" + textBox1.Text + "';";
+            OleDbCommand comando = new OleDbCommand(consulta,conexion);
             OleDbDataReader leedb;
             leedb = comando.ExecuteReader();
             Boolean existereg = leedb.HasRows;
             if (existereg)
             {
-                MessageBox.Show("bienvenido al sistema " + textBox1.Text);
-                Form3 f3 = new Form3();
-                f3.Show();
-
+                MessageBox.Show("bienvenido al sistema" + textBox1.Text);
+                Form2 f2 = new Form2();
+                f2.Show();
+                this.Hide();
 
             }
             else
             {
-                MessageBox.Show(" usuario o contraseña incorreto");
+                MessageBox.Show("usuario o contraseña incorrecto trate de nuevo");
                 return;
             }
             conexion.Close();
