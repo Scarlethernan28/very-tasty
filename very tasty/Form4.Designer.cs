cbdc97d306668c5d37d1973411e9ef3f49361c3c
KEVIN RODRIGO CORCIO BONILLA <USIS017621@ugb.edu.sv>
2021-09-29 10:21:12 -0600

    nuevo menu

--- very tasty/Form4.Designer.cs
+++ very tasty/Form4.Designer.cs
@@ -29,23 +29,18 @@ namespace very_tasty
         /// </summary>
         private void InitializeComponent()
         {
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.label1 = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
-            // 
-            // pictureBox1
-            // 
-            this.pictureBox1.Image = global::very_tasty.Properties.Resources.WhatsApp_Image_2021_09_28_at_11_18_24_AM;
-            this.pictureBox1.Location = new System.Drawing.Point(18, 40);
-            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(336, 224);
-            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.pictureBox1.TabIndex = 0;
-            this.pictureBox1.TabStop = false;
             // 
             // label1
             // 
@@ -59,21 +54,80 @@ namespace very_tasty
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(445, 198);
+            this.button1.Location = new System.Drawing.Point(419, 156);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(91, 42);
+            this.button1.Size = new System.Drawing.Size(100, 42);
             this.button1.TabIndex = 3;
-            this.button1.Text = "selecionar ";
+            this.button1.Text = "seleccionar ";
             this.button1.UseVisualStyleBackColor = true;
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(618, 198);
+            this.button2.Location = new System.Drawing.Point(605, 157);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(95, 41);
             this.button2.TabIndex = 4;
             this.button2.Text = "volver ";
             this.button2.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::very_tasty.Properties.Resources.descarga;
+            this.pictureBox2.Location = new System.Drawing.Point(18, 308);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(336, 201);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 5;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::very_tasty.Properties.Resources.WhatsApp_Image_2021_09_28_at_11_18_24_AM;
+            this.pictureBox1.Location = new System.Drawing.Point(18, 40);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(336, 224);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(375, 318);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(428, 54);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "HAMBURGUESA DE POLLO Y HUEVO FRITO CON QUESO \r\n\r\n\r\n";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(374, 362);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(52, 24);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "2.50\r\n";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(419, 434);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(100, 38);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "seleccionar \r\n";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(605, 434);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(95, 38);
+            this.button4.TabIndex = 9;
+            this.button4.Text = "volver \r\n";
+            this.button4.UseVisualStyleBackColor = true;
             // 
             // Form4
             // 
@@ -81,14 +135,21 @@ namespace very_tasty
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
             this.ClientSize = new System.Drawing.Size(1102, 623);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.pictureBox2);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.pictureBox1);
             this.Font = new System.Drawing.Font("Arial", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "Form4";
             this.Text = "Form4";
+            this.Load += new System.EventHandler(this.Form4_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -101,5 +162,10 @@ namespace very_tasty
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
     }
 }
--- very tasty/Form4.cs
+++ very tasty/Form4.cs
@@ -16,5 +16,15 @@ namespace very_tasty
         {
             InitializeComponent();
         }
+
+        private void Form4_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
