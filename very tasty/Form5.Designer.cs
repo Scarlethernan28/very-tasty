a6ac7a5a14f5618b450c5a0bffa009f3cd3286d4
Genesis Hernández <scarletgenesis2807@gmail.com>
2021-09-29 11:03:34 -0600

    menu de ensalada

--- very tasty/Form5.Designer.cs
+++ very tasty/Form5.Designer.cs
@@ -148,7 +148,7 @@ namespace very_tasty
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.OrangeRed;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
             this.ClientSize = new System.Drawing.Size(843, 483);
             this.Controls.Add(this.button4);
             this.Controls.Add(this.button3);
